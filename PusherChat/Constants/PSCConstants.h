e13b46edbbdb6a240c866b9e9a33deef07e20e4e
Kiet Nguyen <nguyenvankiet.teaching@gmail.com>
2014-05-19 09:59:39 +0700

    Add PSCConstants to save constants string

---
+++ PusherChat/Constants/PSCConstants.h
@@ -0,0 +1,23 @@
+//
+//  PSCConstants.h
+//  PusherChat
+//
+//  Created by Kiet Nguyen on 5/19/14.
+//  Copyright (c) 2014 Kiet Nguyen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#define PUSHER_API_KEY @"a303b94353376eae7485"
+#define PUSHER_APP_ID  @"74518"
+#define PUSHER_API_SECRET  @"43abb3c7a07e0b339e25"
+
+#define FONT_HELVETICAL_REGULAR  @"HelveticaNeue"
+#define FONT_HELVETICAL_LIGHT  @"HelveticaNeue-Light"
+
+#pragma mark - Message Class
+
+extern NSString *const kMessageClassKey;
+extern NSString *const kMessageUserIdSendKey;
+extern NSString *const kMessageUserIDReceiveKey;
+extern NSString *const kMessageContentKey;
---
+++ PusherChat/Constants/PSCConstants.m
@@ -0,0 +1,20 @@
+//
+//  PSCConstants.m
+//  PusherChat
+//
+//  Created by Kiet Nguyen on 5/19/14.
+//  Copyright (c) 2014 Kiet Nguyen. All rights reserved.
+//
+
+#import "PSCConstants.h"
+
+#pragma mark - Message Class
+
+// Class key
+NSString *const kMessageClassKey = @"Message";
+
+// Field keys
+NSString *const kMessageUserIdSendKey = @"UserId_Send";
+NSString *const kMessageUserIDReceiveKey = @"UserId_Receive";
+NSString *const kMessageContentKey = @"Content";
+
