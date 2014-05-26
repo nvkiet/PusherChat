ce7695eca66e8ab3795f02d8c562314930ba4a41
Kiet Nguyen <nguyenvankiet.teaching@gmail.com>
2014-05-26 10:59:10 +0700

    Fix bubble cell is located near the navigation bar

--- PusherChat/UIViews/BubbleTableView/PSCBubbleCell.m
+++ PusherChat/UIViews/BubbleTableView/PSCBubbleCell.m
@@ -60,7 +60,7 @@
     
     [self.customView removeFromSuperview];
     self.customView = data.view;
-    self.customView.frame = CGRectMake(x + data.insets.left - (type == BubbleTypeSomeoneElse ? 2 : 0), y + data.insets.top, width, height); 
+    self.customView.frame = CGRectMake(x + data.insets.left - (type == BubbleTypeSomeoneElse ? 2 : 0), y + data.insets.top + 5, width, height);
     [self.contentView addSubview:self.customView];
     
     if (type == BubbleTypeSomeoneElse){
@@ -70,7 +70,7 @@
         self.bubbleImageView.image = [[UIImage imageNamed:@"ChatBubble_send.png"] stretchableImageWithLeftCapWidth:15 topCapHeight:14];
     }
     
-    self.bubbleImageView.frame = CGRectMake(x, y, width + data.insets.left + data.insets.right, height + data.insets.top + data.insets.bottom);
+    self.bubbleImageView.frame = CGRectMake(x, y + 5, width + data.insets.left + data.insets.right, height + data.insets.top + data.insets.bottom);
 }
 
 @end
